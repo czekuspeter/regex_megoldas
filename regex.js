ed5dbd43c142dc93383a02daff68eeebecdedcf9
Czékus Péter <144217254+czekuspeter@users.noreply.github.com>
2024-01-15 09:16:29 +0100

    Add files via upload

---
+++ regex.js
@@ -0,0 +1,19 @@
+// Regex megoldasok
+
+//1.
+var Regex_Pattern = 'hackerrank'; 
+
+//2.
+var Regex_Pattern = /^(\S{3}\.)(\S{3}\.){2}(\S{3})$/g 
+
+//3.
+var Regex_Pattern = /\d{2}\D{1}\d{2}\D{1}\d{4}/; 
+
+//4.
+var Regex_Pattern = /\S{2}\s\S{2}\s\S{2}/ 
+
+//5.
+var Regex_Pattern = /\w{3}\W\w{10}\W\w{3}/ 
+
+//6.
+var Regex_Pattern = /^\d\w{4}\.$/; 
